89ac59a5b0036b60e0dd3de0aa8726e8070bc161
wolizhiquana <wolizhiquana@gmail.com>
2023-02-25 19:27:44 +0800

    getURLsFromHTML function and tests

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,7 +1,7 @@
-import { normalizeURL } from "./crawl";
+import { getURLsFromHTML, normalizeURL } from "./crawl";
 
-describe("normalizeURL function", () => {
-  it("带有协议的url", () => {
+describe("normalizeURL", () => {
+  it("输入有协议的url", () => {
     const input = "https://blog.boot.dev";
     const actual = normalizeURL(input);
     const expected = "blog.boot.dev";
@@ -9,7 +9,7 @@ describe("normalizeURL function", () => {
     expect(actual).toEqual(expected);
   });
 
-  it("带有路径的url", () => {
+  it("输入有路径的url", () => {
     const input = "https://blog.boot.dev/path";
     const actual = normalizeURL(input);
     const expected = "blog.boot.dev/path";
@@ -17,7 +17,7 @@ describe("normalizeURL function", () => {
     expect(actual).toEqual(expected);
   });
 
-  it("以斜杠结尾的url", () => {
+  it("输入以斜杠结尾的url", () => {
     const input = "https://blog.boot.dev/";
     const actual = normalizeURL(input);
     const expected = "blog.boot.dev";
@@ -25,11 +25,87 @@ describe("normalizeURL function", () => {
     expect(actual).toEqual(expected);
   });
 
-  it("有大写的url", () => {
+  it("输入有大写的url", () => {
     const input = "https://BLOG.boot.dev/path/";
     const actual = normalizeURL(input);
     const expected = "blog.boot.dev/path";
 
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("getURLsFromHTML", () => {
+  it("绝对路径", () => {
+    const inputHTMLBody = `
+<html>
+  <body>
+    <a href="https://blog.boot.dev/">
+      Boot.dev Blog
+    </a>
+  </body>
+</html>
+`;
+    const inputBaseURL = "https://blog.boot.dev";
+
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.boot.dev/"];
+    expect(actual).toEqual(expected);
+  });
+
+  it("相对路径", () => {
+    const inputHTMLBody = `
+<html>
+  <body>
+    <a href="/path/">
+      Boot.dev Blog
+    </a>
+  </body>
+</html>
+`;
+    const inputBaseURL = "https://blog.boot.dev";
+
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.boot.dev/path/"];
+    expect(actual).toEqual(expected);
+  });
+
+  it("相对路径和绝对路径", () => {
+    const inputHTMLBody = `
+<html>
+  <body>
+    <a href="https://blog.boot.dev/path1/">
+      Boot.dev Blog
+    </a>
+    <a href="/path2/">
+      Boot.dev Blog
+    </a>
+  </body>
+</html>
+`;
+    const inputBaseURL = "https://blog.boot.dev";
+
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = [
+      "https://blog.boot.dev/path1/",
+      "https://blog.boot.dev/path2/",
+    ];
+    expect(actual).toEqual(expected);
+  });
+
+  it("不应该包括无效路径", () => {
+    const inputHTMLBody = `
+<html>
+  <body>
+    <a href="invalid">
+      Boot.dev Blog
+    </a>
+  </body>
+</html>
+`;
+    const inputBaseURL = "https://blog.boot.dev";
+
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected: string[] = [];
     expect(actual).toEqual(expected);
   });
 });
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,3 +1,32 @@
+import { JSDOM } from "jsdom";
+
+export function getURLsFromHTML(htmlBody: string, baseURL: string) {
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+
+  const urls: string[] = [];
+  linkElements.forEach((element) => {
+    const linkUrl = element.href;
+
+    if (linkUrl[0] === "/") {
+      try {
+        const urlObj = new URL(baseURL + linkUrl);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`error with relative url: ${error}`);
+      }
+    } else
+      try {
+        const urlObj = new URL(linkUrl);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`error with absolute url: ${error}`);
+      }
+  });
+
+  return urls;
+}
+
 export function normalizeURL(urlString: string) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
